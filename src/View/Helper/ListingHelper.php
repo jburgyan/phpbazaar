8f363c192b634ae9767833a15da40974c4cdc50b
joe2night <joe2night@bitbucket.org>
2019-08-30 10:04:40 +0200

    documented ListingHelper

--- src/View/Helper/ListingHelper.php
+++ src/View/Helper/ListingHelper.php
@@ -8,12 +8,20 @@ namespace App\View\Helper;
 use Cake\View\Helper;
 use Cake\Utility\Inflector;
 
+/**
+ * Class ListingHelper
+ * @package App\View\Helper
+ */
 class ListingHelper extends Helper\HtmlHelper {
 
+	/**
+	 * @param array  $pricejson
+	 * @param string $contractType
+	 */
 	public function price( array $pricejson, $contractType ) {
 		$html = '';
 		if ( $pricejson['amount'] || $contractType != 'CRYPTOCURRENCY' ) {
-			$html .= $pricejson['amount']/100;
+			$html .= $pricejson['amount'] / 100;
 		}
 		$html .= ' ' . $pricejson['currencyCode'];
 		if ( $contractType == 'CRYPTOCURRENCY' ) {
@@ -22,6 +30,13 @@ class ListingHelper extends Helper\HtmlHelper {
 		echo trim( $html );
 	}
 
+	/**
+	 * @param string     $categories
+	 * @param string     $what
+	 * @param string     $controller
+	 * @param string     $action
+	 * @param int|string $id
+	 */
 	public function arrtolinks( $categories, $what = 'c', $controller = 'Listings', $action = 'index', $id = null ) {
 		$categories = trim( $categories, "{}" );
 		$categories = explode( ",", $categories );
@@ -43,78 +58,88 @@ class ListingHelper extends Helper\HtmlHelper {
 		}
 	}
 
-	public function arrtolist(array $elements, $print = true) {
+	/**
+	 * @param array $elements
+	 * @param bool  $print
+	 *
+	 * @return string|void
+	 */
+	public function arrtolist( array $elements, $print = true ) {
 		$list = '';
-		foreach ($elements as $label => $element) {
-			$label = Inflector::humanize(preg_replace('/(?<!\ )[A-Z]/', ' $0', $label));
-			if(is_array($element) && !empty($element)) {
-				if(is_numeric($label)) {
-					$list .= '<li>'.$this->arrtolist($element, false).'</li>';
+		foreach ( $elements as $label => $element ) {
+			$label = Inflector::humanize( preg_replace( '/(?<!\ )[A-Z]/', ' $0', $label ) );
+			if ( is_array( $element ) && ! empty( $element ) ) {
+				if ( is_numeric( $label ) ) {
+					$list .= '<li>' . $this->arrtolist( $element, false ) . '</li>';
 				} else {
-					$list .= '<li><strong>'.$label.'</strong>: '.$this->arrtolist($element, false).'</li>';
+					$list .= '<li><strong>' . $label . '</strong>: ' . $this->arrtolist( $element, false ) . '</li>';
 				}
-			} elseif(!empty($element)) {
-				if(filter_var($element, FILTER_VALIDATE_URL)) {
-					if(!strstr($element, 'http')) {
-						$element = 'http://'.$element;
+			} elseif ( ! empty( $element ) ) {
+				if ( filter_var( $element, FILTER_VALIDATE_URL ) ) {
+					if ( ! strstr( $element, 'http' ) ) {
+						$element = 'http://' . $element;
 					}
-					$element = '<a href="'.$element.'" target="_blank">'.$element.'</a>';
-				} elseif(filter_var($element, FILTER_VALIDATE_EMAIL)) {
-					$element = '<a href="mailto:'.$element.'">'.$element.'</a>';
+					$element = '<a href="' . $element . '" target="_blank">' . $element . '</a>';
+				} elseif ( filter_var( $element, FILTER_VALIDATE_EMAIL ) ) {
+					$element = '<a href="mailto:' . $element . '">' . $element . '</a>';
 				} else {
-					$element = ucwords($element);
+					$element = ucwords( $element );
 				}
-				$list .= '<li><strong>'.$label.'</strong>: '.$element.'</li>';
+				$list .= '<li><strong>' . $label . '</strong>: ' . $element . '</li>';
 			}
 		}
-		if(!empty($list)) {
-			$list = '<ul>'.$list.'</ul>';
-			if($print) {
+		if ( ! empty( $list ) ) {
+			$list = '<ul>' . $list . '</ul>';
+			if ( $print ) {
 				echo $list;
 			} else {
 				return $list;
 			}
 		}
+
 		return '';
 	}
 
-	public function printimages($images, $size = 'large', $options = array(), $print = true) {
-		if(!is_array($images)) {
-			$images = $this->pg_array_parse($images);
+	/**
+	 * @param array|string $images
+	 * @param string       $size
+	 * @param array        $options
+	 */
+	public function printimages( $images, $size = 'large', $options = array() ) {
+		if ( ! is_array( $images ) ) {
+			$images = $this->pg_array_parse( $images );
 		}
-		if(!empty($images) && !isset($images[0])) {
-			$images = array($images);
+		if ( ! empty( $images ) && ! isset( $images[0] ) ) {
+			$images = array( $images );
 		}
-		if(!empty($images)) {
-			foreach ($images as $image) {
-				if(!empty($image[$size])) {
-					$filename = $image[$size].'_'.$size;
-					$obdir = $path = WWW_ROOT.'img'.DS.'ob';
-					if(!is_dir($obdir)) {
-						mkdir($obdir);
+		if ( ! empty( $images ) ) {
+			foreach ( $images as $image ) {
+				if ( ! empty( $image[ $size ] ) ) {
+					$filename = $image[ $size ] . '_' . $size;
+					$obdir    = $path = WWW_ROOT . 'img' . DS . 'ob';
+					if ( ! is_dir( $obdir ) ) {
+						mkdir( $obdir );
 					}
-					$path = $obdir.DS.$filename;
-					if(!is_file($path)) {
-						$imagedata = @file_get_contents("http://localhost:4002/ipfs/".$image[$size]."?usecache=false");
-						if(!empty($imagedata)) {
-							file_put_contents($path, $imagedata);
+					$path = $obdir . DS . $filename;
+					if ( ! is_file( $path ) ) {
+						$imagedata = @file_get_contents( "http://localhost:4002/ipfs/" . $image[ $size ] . "?usecache=false" );
+						if ( ! empty( $imagedata ) ) {
+							file_put_contents( $path, $imagedata );
 						}
 					}
-					if(is_file($path)) {
-						if($print) {
-							echo $this->image('ob/'.$filename, $options);
-						} else {
-							$this->Url->image('ob/'.$filename, $options);
-						}
+					if ( is_file( $path ) ) {
+						echo $this->image( 'ob/' . $filename, $options );
 					}
 				}
 			}
 		}
-		if(!$print) {
-			return null;
-		}
 	}
 
+	/**
+	 * @param string $literal
+	 *
+	 * @return array|null
+	 */
 	public function pg_array_parse( $literal ) {
 		if ( $literal == '' ) {
 			return null;
@@ -122,9 +147,9 @@ class ListingHelper extends Helper\HtmlHelper {
 		preg_match_all( '/(?<=^\{|,)(([^,"{]*)|\s*"((?:[^"\\\\]|\\\\(?:.|[0-9]+|x[0-9a-f]+))*)"\s*)(,|(?<!^\{)(?=\}$))/i', $literal, $matches, PREG_SET_ORDER );
 		$values = [];
 		foreach ( $matches as $match ) {
-			$value = $match[3] != '' ? stripcslashes( $match[3] ) : ( strtolower( $match[2] ) == 'null' ? null : $match[2] );
-			$arrvalue = @json_decode($value, true);
-			if(empty($arrvalue) && !empty($value)) {
+			$value    = $match[3] != '' ? stripcslashes( $match[3] ) : ( strtolower( $match[2] ) == 'null' ? null : $match[2] );
+			$arrvalue = @json_decode( $value, true );
+			if ( empty( $arrvalue ) && ! empty( $value ) ) {
 				$values[] = $value;
 			} else {
 				$values[] = $arrvalue;
